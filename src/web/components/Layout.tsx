79af1e8e0fa3cd6ab6a0828dca881f1f3b6d7e58
Santiago Isola <isolasantiago@gmail.com>
2024-11-15 02:43:09 -0300

    Fix fixed body heigth

--- src/web/components/Layout.tsx
+++ src/web/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export function Layout(props: LayoutProps) {
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 h-screen rootContainer">
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 rootContainer">
             <header>
                 <div className="max-w-7xl mx-auto py-6 px-4">
                     <h1 className="mt-4 text-center text-3xl font-bold text-gray-900 dark:text-gray-100">
