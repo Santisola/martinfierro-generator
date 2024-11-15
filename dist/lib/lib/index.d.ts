9c243085beb0ab33c02683cdbf30b0664ade3437
Santiago Isola <isolasantiago@gmail.com>
2024-11-15 01:47:03 -0300

    Add keywords, change homepage in package.json

--- dist/lib/lib/index.d.ts
+++ dist/lib/lib/index.d.ts
@@ -1,2 +1,8 @@
+/**
+ * Generates a string of Jose Hernandez's epic poem "Martin Fierro"
+ *
+ * @param stringLength The length of the requested Martin Fierro's string.
+ * @returns An EPIC string by El Gaucho Martín Fierro. | Empty string in case of an error
+ */
 declare const generate: (stringLength: number) => string;
 export { generate };
